2d6988522a00619aacce2561d6f7ccbf4bf59f44
patdevincentis <devincentis529@gmail.com>
2018-12-11 13:29:56 -0500

    built sign in and sign up routes and components as well as adjusted navbar and header with minmax width on the grid

---
+++ src/components/auth/signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SignIn extends Component {
+  render() {
+    return (
+      <div className="sign-in">
+        <h1> this is the sign in section</h1>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
---
+++ src/components/auth/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+  render() {
+    return (
+      <div className="sign-up">
+        <h1> this is the sign up section</h1>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
