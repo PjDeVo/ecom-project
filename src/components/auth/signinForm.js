82e7dcd97ae49d4f0736f0c8583d975b202be25c
patdevincentis <devincentis529@gmail.com>
2018-12-12 17:02:57 -0500

    built the details component for quicklinks

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -3,38 +3,61 @@ import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 
 import { FormInput, FormButton } from "../formFields";
+import Details from "../details";
+
+import history from "../../history";
 
 class SignInForm extends Component {
   render() {
     const { className, handleSubmit } = this.props;
+    const links = [
+      {
+        _id: 0,
+        title: "Not registered? Create account here",
+        onClick: () => history.push("/signup")
+      },
+      {
+        _id: 1,
+        title: "Forgot account email?",
+        onClick: () => console.log("forgot email")
+      },
+      {
+        _id: 2,
+        title: "Forgot password?",
+        onClick: () => console.log("forgot password")
+      }
+    ];
     return (
       <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
         <Field
           className="sign-in-form__email"
+          type="email"
+          title="Email"
+          placeholder="Email"
           name="email"
           component={FormInput}
-          title="Email"
-          type="email"
-          placeholder="Email"
         />
         <Field
           className="sign-in-form__password"
+          type="password"
+          title="Password"
+          placeholder="Password"
           name="password"
           component={FormInput}
-          title="Password"
-          type="password"
-          placeholder="Password"
         />
-
-        <div className="sign-in-form__line"> </div>
-
+        <div className="sign-in-form__line" />
         <Field
-          onClick={() => console.log("tryna submit")}
           className="sign-in-form__login"
+          onClick={() => console.log("tryna submit")}
+          type="submit"
+          title="Login"
           name="login"
           component={FormButton}
-          title="Login"
-          type="submit"
+        />
+        <Details
+          className="sign-in-form__details"
+          title="QuickLinks"
+          links={links}
         />
       </form>
     );
