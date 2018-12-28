642fe3a72cb275123332f16a544b29e42a9da219
patdevincentis <devincentis529@gmail.com>
2018-12-28 15:38:09 -0500

    built accountInformation form template

--- src/components/account/accountInformation.js
+++ src/components/account/accountInformation.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import PageTitle from "../pageTitle";
+import AccountInformationForm from "./accountInformationForm";
 
 class AccountInformation extends Component {
+  onSubmit = fields => {
+    console.log(fields);
+  };
+
   render() {
-    return <div>Account Information</div>;
+    return (
+      <div className="account-information">
+        <PageTitle
+          className="account-information"
+          title="Account Information"
+        />
+        <AccountInformationForm
+          onSubmit={this.onSubmit}
+          className="sign-in__form"
+        />
+      </div>
+    );
   }
 }
 
