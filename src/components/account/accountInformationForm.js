4573e67f210238e8afb3e00c2b08a7262e7ca7ee
patdevincentis <devincentis529@gmail.com>
2018-12-28 17:43:06 -0500

    finished account information form

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -73,6 +73,8 @@ class AccountInformationForm extends Component {
           name="zipcode"
           component={FormInput}
         />
+        <div key={3} className="account-information-form__line" />
+
         {this.state.showPasswords ? (
           [
             <Field
@@ -101,6 +103,26 @@ class AccountInformationForm extends Component {
               placeholder="Confirm Password"
               name="confirm"
               component={FormInput}
+            />,
+
+            <Field
+              key={4}
+              className="account-information-form__update-information"
+              onClick={() => this.setState({ showPasswords: false })}
+              type="submit"
+              title="Update Information"
+              name="update-information"
+              component={FormButton}
+            />,
+            <Field
+              key={5}
+              className="account-information-form__cancel"
+              onClick={() => this.setState({ showPasswords: false })}
+              type="button"
+              title="Cancel"
+              name="Cancel"
+              short={true}
+              component={FormButton}
             />
           ]
         ) : (
