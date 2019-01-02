66eb3f49c778e4549d946d87ffda781003b3d96d
patdevincentis <devincentis529@gmail.com>
2019-01-01 23:03:01 -0500

    cart animations and cart button in shop component

--- src/components/shop/shopProduct.js
+++ src/components/shop/shopProduct.js
@@ -3,6 +3,15 @@ import Quantity from "../quantity";
 import GreenPriceTag from "../greenPriceTag";
 
 class ShopProduct extends Component {
+  handleAddToCart = () => {
+    if (
+      document.getElementById("shop-cart").classList.contains("cart-hidden")
+    ) {
+      document.getElementById("shop-cart").classList.remove("cart-hidden");
+    } else {
+      document.getElementById("shop-cart").classList.add("cart-hidden");
+    }
+  };
   render() {
     const { _id, title, description, price } = this.props;
     return (
@@ -12,7 +21,7 @@ class ShopProduct extends Component {
             className="shop-product__front__image"
             src="http://via.placeholder.com/220x220"
           />
-          <div className="shop-product__front__title"> {title} </div>
+          s<div className="shop-product__front__title"> {title} </div>
         </div>
 
         <div className="shop-product__back">
@@ -20,7 +29,13 @@ class ShopProduct extends Component {
           <div className="shop-product__back__description">{description}</div>
           <GreenPriceTag className="shop-product__back__price" title={price} />
           <Quantity className="shop-product__back__quantity" quantity={1} />
-          <div className="shop-product__back__add-to-cart"> Add To Cart</div>
+          <a
+            onClick={this.handleAddToCart}
+            className="shop-product__back__add-to-cart"
+          >
+            {" "}
+            Add To Cart
+          </a>
         </div>
       </div>
     );
