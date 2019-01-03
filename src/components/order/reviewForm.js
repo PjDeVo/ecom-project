79c0f9ee87956c90c0240911e77e0e5a9dbf37a2
patdevincentis <devincentis529@gmail.com>
2019-01-02 21:48:45 -0500

    built review from details

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-
 import { reduxForm, Field } from "redux-form";
 
-import { FormButton } from "../formFields";
-
 import history from "../../history";
+
+import { FormButton } from "../formFields";
 import ReviewProducts from "./reviewProducts";
 
 class ReviewForm extends Component {
@@ -14,9 +13,9 @@ class ReviewForm extends Component {
     return (
       <form onSubmit={handleSubmit} className={`${className} review-form`}>
         <div className="review-form__legend">
-          <div className="review-form__legend__name">Name </div>
-          <div className="review-form__legend__quantity">Quantity </div>
-          <div className="review-form__legend__price">Price </div>
+          <div className="review-form__legend__name">Name</div>
+          <div className="review-form__legend__quantity">Quantity</div>
+          <div className="review-form__legend__price">Price</div>
         </div>
         <ReviewProducts className="review-form__products" />
         <div className="review-form__line" />
@@ -37,6 +36,29 @@ class ReviewForm extends Component {
           short={true}
           component={FormButton}
         />
+
+        <div className="review-form__details review-details">
+          <div className="review-details__subtotal review-detail">
+            <div className="review-detail__title">Subtotal</div>
+            <div className="review-detail__price">$10</div>
+          </div>
+          <div className="review-details__subtotal review-detail">
+            <div className="review-detail__title">Tax</div>
+            <div className="review-detail__price">$12</div>
+          </div>
+          <div className="review-details__subtotal review-detail">
+            <div className="review-detail__title">Shipping</div>
+            <div className="review-detail__price">$0.00</div>
+          </div>
+          <div className="review-details__total review-detail review-detail-green">
+            <div className="review-detail__title review-detail-green__title">
+              Total
+            </div>
+            <div className="review-detail__price review-detail-green__price">
+              $0
+            </div>
+          </div>
+        </div>
       </form>
     );
   }
